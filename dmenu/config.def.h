083ccc5ae01446273e1dd58e1b8ecfea895ca592
faye <0hachi@pm.me>
2023-01-02 12:38:47 +0100

    feat: theme patch

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+    "Source Code Pro:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    /*     fg         bg       */
+    [SchemeNorm] = { "#fff", "#26292C" },
+    [SchemeSel] = { "#fff", "#2e3135" },
+    [SchemeOut] = { "#fff", "#2e3135" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
